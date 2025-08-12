90935d4aaa2d4f6bc1acd192ed0b5b3e72462618
Harry Burman <burmanharry@gmail.com>
2025-08-12 15:15:08 -0700

    Save layout and successful local build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Crisp } from "./(lib)/crisp";
 
 export const metadata = {
   title: "CleanIngredients",
@@ -12,29 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
-import "./globals.css";
-import { Crisp } from "./(lib)/crisp";
-
-export const metadata = { title: "CleanIngredients" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
       <body>
         <Crisp />
         {children}
-        import Script from "next/script";
-// ...
-<Script
-  defer
-  data-domain="yourdomain.com" // or vercel.app domain
-  src="https://plausible.io/js/script.js"
-/>
-
       </body>
     </html>
   );
