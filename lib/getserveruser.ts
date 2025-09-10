91b794317557968af9948daebc286b9966e78f38
Harry Burman <burmanharry@gmail.com>
2025-09-10 12:17:18 -0700

    Remove getserveruser.ts to fix supabaseServer import error

--- lib/getserveruser.ts
+++
@@ -1,7 +0,0 @@
-import { supabaseServer } from '@/lib/supabaseServer';
-
-export async function getServerUser() {
-  const supabase = await supabaseServer();
-  const { data: { user } } = await supabase.auth.getUser();
-  return user ?? null;
-}
