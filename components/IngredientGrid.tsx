763b69b44e81fca93db0e8b9149cbf90220a628a
Harry Burman <burmanharry@gmail.com>
2025-09-11 21:26:38 -0700

    Export IngredientTile and type IngredientGrid props

--- components/IngredientGrid.tsx
+++ components/IngredientGrid.tsx
@@ -1,11 +1,11 @@
 // components/IngredientGrid.tsx
 import IngredientCard, { type IngredientTile } from "./IngredientCard";
 
-export default function IngredientGrid({ items }: { items: IngredientTile[] }) {
+export default function IngredientGrid({ items }: { items: ReadonlyArray<IngredientTile> }) {
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {items.map((it) => (
-        <IngredientCard key={it.id} {...it} />
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {items.map((item) => (
+        <IngredientCard key={String(item.slug ?? item.id)} item={item} />
       ))}
     </div>
   );
